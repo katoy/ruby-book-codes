79c2bc2a55b9c0aab87ae12163498ca6ae0337f7
kato_youichi <youichikato@gmail.com>
2019-01-12 19:20:34 +0900

    fizz_buzz*_spec.rb の更新

--- ruby-book/spec/lib/fizz_buzz_01_spec.rb
+++ ruby-book/spec/lib/fizz_buzz_01_spec.rb
@@ -1,34 +1,23 @@
+# frozen_string_literal: true
+
 require './spec/spec_helper'
 require './lib/fizz_buzz'
 
 RSpec.describe 'fizz_buzz' do
-  subject{fizz_buzz(arg)}
-  context 'arg is' do
-    context '1' do
-      let(:arg){1}
-      it{is_expected.to eq '1'}
-    end
-  end
+  subject { fizz_buzz(arg) }
 
-  context 'arg_1 is' do
-    [1, 2, 3, 5, 15].each do |x|
-      context "#{x}" do
-        let(:arg){x}
-        let(:expect_ret) do
-          case x
-          when 15
-            'FizzBuzz'
-          when 3
-            'Fizz'
-          when 5
-            'Buzz'
-          else
-            x.to_s
-          end
-        end
+  TEST_CASES = [
+    [1, '1'],
+    [2, '2'],
+    [3, 'Fizz'],
+    [5, 'Buzz'],
+    [15, 'FizzBuzz']
+  ].each do |test_case|
+    context "arg is #{test_case[0]}" do
+      let(:arg) { test_case[0] }
+      let(:expect_result) { test_case[1] }
 
-        it{is_expected.to eq expect_ret}
-      end
+      it { is_expected.to eq expect_result }
     end
   end
 end
